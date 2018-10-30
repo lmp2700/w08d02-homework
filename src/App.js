37951c3e90dcc13241a07ef84cb1867a62021647
Liz Steak <lizmaepark@gmail.com>
2018-10-30 17:57:34 -0600

    created login page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from './Login'
+
+// Create login functionality in which when you land on the app, you see a login component, 
+// and then it switches, to your main container component after you are logged in.
+// Inside of your main component you will render at least three properties from your API.
+// Style your app with Bootstrap make it really nice looking.
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      logged: false,
+      username: ''
+    }
+  }
+  handleLogin = (username, isLoggedIn) => {
+    this.setState({
+      username: username,
+      logged: isLoggedIn
+    })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>NASA Photos Here</h1><br/>
+        {this.state.logged ? <Login username={this.state}/> : <Login handleLogin={this.handleLogin}/>} 
       </div>
     );
   }
